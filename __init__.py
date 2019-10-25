c0b8f8df9db7421dace66741de034c302e7a66a9
Jared Cobb <github@jaredcobb.com>
2019-10-25 12:04:44 -0400

    Debug

--- __init__.py
+++ __init__.py
@@ -7,6 +7,9 @@ class CryptoMarkets(MycroftSkill):
         self.url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        self.register_entity_file("coin.entity")
+
     @intent_file_handler('price.crypto.intent')
     def handle_price_crypto(self, message):
         coin = message.data.get('coin')
