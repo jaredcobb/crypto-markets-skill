7fefccfab15f68b9096872a82ac60019f916fda0
Jared Cobb <github@jaredcobb.com>
2019-10-25 15:39:42 -0400

    Testing

--- __init__.py
+++ __init__.py
@@ -37,9 +37,9 @@ class CryptoMarkets(MycroftSkill):
                             price_change_direction = 'down'
 
                         current_price = "{0:.4f}".format(item['current_price'])
-                        price_change_24 = "{0:.4f}".format(item['price_change_24'])
+                        price_change_24h = "{0:.4f}".format(item['price_change_24h'])
 
-                        self.speak_dialog('price.crypto', {'coin': item['name'], 'current_price': current_price, 'price_change_direction': price_change_direction, 'price_change_24h': price_change_24})
+                        self.speak_dialog('price.crypto', {'coin': item['name'], 'current_price': current_price, 'price_change_direction': price_change_direction, 'price_change_24h': price_change_24h})
 
                 if match == False:
                     self.speak_dialog('missing.crypto')
