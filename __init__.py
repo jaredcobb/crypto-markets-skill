3e8258a39697759e364460a3f8390fae35dcfd87
Jared Cobb <github@jaredcobb.com>
2019-10-25 10:06:41 -0400

    Fix logging

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ import json
 
 class CryptoMarkets(MycroftSkill):
     def __init__(self):
-        self.log.info('Initializing Crypto Markets Skill...')
         self.url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
         MycroftSkill.__init__(self)
 
@@ -15,11 +14,14 @@ class CryptoMarkets(MycroftSkill):
             self.log.info('Identified the coin request. Calling the CoinGecko API for ' + str(coin))
             apiResponse = requests.get(self.url)
             if apiResponse.ok:
+                self.log.info('API Response was OK...')
                 apiData = json.loads(apiResponse.content)
                 self.speak_dialog('price.crypto', {'coin': coin})
             else:
+                self.log.info('API Response Failed...')
                 self.speak_dialog('missing.crypto')
         else:
+            self.log.info('Could not determine the requested coin...')
             self.speak_dialog('missing.crypto')
 
 def create_skill():
