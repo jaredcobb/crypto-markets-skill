9fbddef4530020ff991a4a8d227a88da3c8f28bc
Jared Cobb <github@jaredcobb.com>
2019-10-25 09:27:01 -0400

    Adding specific intent files

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-
 class CryptoMarkets(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('markets.crypto.intent')
-    def handle_markets_crypto(self, message):
-        self.speak_dialog('markets.crypto')
-
+    @intent_file_handler('price.crypto.intent')
+    def handle_price_crypto(self, message):
+        coin = message.data.get('coin')
+        if coin is not None:
+            self.speak_dialog('price.crypto', {'coin': coin})
+        else:
+            self.speak_dialog('missing.crypto')
 
 def create_skill():
     return CryptoMarkets()
-
