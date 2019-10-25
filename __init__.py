b0c3bffe6c4b02f79281d9abc47d1147b0ce94f8
Jared Cobb <github@jaredcobb.com>
2019-10-25 09:56:45 -0400

    Fixed logging and typo

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import json
 
 class CryptoMarkets(MycroftSkill):
     def __init__(self):
-        print('Initializing Crypto Markets Skill...')
+        self.log.info('Initializing Crypto Markets Skill...')
         self.url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
         MycroftSkill.__init__(self)
 
@@ -12,10 +12,10 @@ class CryptoMarkets(MycroftSkill):
     def handle_price_crypto(self, message):
         coin = message.data.get('coin')
         if coin is not None:
-            print('Identified the coin request. Calling the CoinGecko API...')
+            self.log.info('Identified the coin request. Calling the CoinGecko API for ' + str(coin))
             apiResponse = requests.get(self.url)
             if apiResponse.ok:
-                apiData = json.loads(myResponse.content)
+                apiData = json.loads(apiResponse.content)
                 self.speak_dialog('price.crypto', {'coin': coin})
             else:
                 self.speak_dialog('missing.crypto')
